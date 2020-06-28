5ac1a6b809266fd599bdf9bbccab9ef6644209ce
Ajeet Kumar <ajeet2511@live.com>
2020-06-28 08:10:20 +0530

    Initial Commit

---
+++ Simple App To Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Simple App To Test
+//
+//  Created by Ajeet Kumar on 28/06/20.
+//  Copyright © 2020 DigitalCurry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Simple App To TestTests/Simple_App_To_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  Simple_App_To_TestTests.swift
+//  Simple App To TestTests
+//
+//  Created by Ajeet Kumar on 28/06/20.
+//  Copyright © 2020 DigitalCurry. All rights reserved.
+//
+
+import XCTest
+@testable import Simple_App_To_Test
+
+class Simple_App_To_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
